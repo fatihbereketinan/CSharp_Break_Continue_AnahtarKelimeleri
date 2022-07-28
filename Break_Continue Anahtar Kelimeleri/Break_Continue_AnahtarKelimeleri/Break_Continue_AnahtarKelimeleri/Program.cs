142bbbe5df27604d220176c59966ec148306cf77
fatihbereketinan <fatihbereketinan@gmail.com>
2022-07-28 22:57:10 +0300

    CSharp_Break_Continue_AnahtarKelimeleri

---
+++ Break_Continue Anahtar Kelimeleri/Break_Continue_AnahtarKelimeleri/Break_Continue_AnahtarKelimeleri/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Break_Continue_AnahtarKelimeleri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)  //Break anahtar kelimesi koşul sağlanınca döngüden çıkmayı sağladı.
+            {
+                if (i==6)
+                {
+                    break;
+                }
+                Console.WriteLine("Break anahtar kelimesi ile say: "+i);
+            }
+
+            for (int i = 0; i < 10; i++) //Continue anahtar kelimesi koşul sağlanınca bir tur atladı sonra saymaya devam etti.
+            {
+                if (i==6)
+                {
+                    continue;
+                }
+                Console.WriteLine("Continue anahtar kelimesi ile say: "+i);
+            }
+        }
+    }
+}
